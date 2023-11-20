0512abbc84a3a80fa978e09f3fb24e1d301a2bc3
jerryChas <jerry.olsson@chasacademy.se>
2023-11-20 17:23:03 +0100

    fetch cv.json

--- js/cv.js
+++ js/cv.js
@@ -4,6 +4,7 @@ const cvContent = document.querySelector('.cv-content')
 
 async function getCV() {
     console.log('inside getCV')
+    const cvResponse = await fetch('./data/cv.json')
 }
 
 getCV()
