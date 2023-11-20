2e9cd87536a24ff45b9784110a9cd8743a7ecf0b
jerryChas <jerry.olsson@chasacademy.se>
2023-11-20 17:35:31 +0100

    checks if response is OK

--- js/cv.js
+++ js/cv.js
@@ -1,11 +1,27 @@
 //hämta element
 const cvContent = document.querySelector('.cv-content')
 
+//anropar funktion
+getCV()
 
+//Hämta json
 async function getCV() {
     console.log('inside getCV')
     const cvResponse = await fetch('./data/cv.json')
+
+    //Kontrollera att responsen är OK
+    if (cvResponse.ok) {
+        const json = await cvResponse.json();
+        console.log(json)
+
+
+    }
+
+
+
+
+
 }
 
-getCV()
+
 
