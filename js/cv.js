4c734f85a755e2a3ee9f6d883c112ccf09fd1b64
jerryChas <jerry.olsson@chasacademy.se>
2023-11-21 15:06:28 +0100

    inserted experience

--- js/cv.js
+++ js/cv.js
@@ -4,6 +4,19 @@ const cvContent = document.querySelector('.cv-content')
 
 //*FUNKTIONER
 
+function createElementFromJSON(jsonSource, classNameDiv, classNameP ){
+     //skapa container som ska hålla informationen
+     const divContainer = document.createElement('div')
+     divContainer.classList.add(classNameDiv);
+
+     //Hämtar data om speakers och skickar in i Diven per key
+     const pContainer = document.createElement('p');
+     pContainer.classList.add(classNameP);
+     // experienceTitle.textContent = object.experience.title;
+     
+     console.log(jsonSource)
+    //  container.appendChild();
+}
 
 
 //Hämta json
@@ -17,22 +30,36 @@ async function getCV() {
         console.log(typeof jsonData)
         console.log(jsonData)
         
-        jsonData.forEach(function(object){
 
-        //skapa container som ska hålla informationen
-        const experienceDiv = document.createElement('div')
-        experienceDiv.classList.add('experience_div');
+        
+        jsonData.experience.forEach(function(exp){
+
+   
+        const experienceDiv = document.querySelector('.experience_div')
+        
 
         //Hämtar data om speakers och skickar in i Diven per key
-        const experienceTitle = document.createElement('p');
+        const experienceTitle = document.createElement('h3');
         experienceTitle.classList.add('experience-title');
-        experienceTitle.textContent = object.experience.title;
+        experienceTitle.textContent = exp.title;
         experienceDiv.appendChild(experienceTitle);
+
+        const description = document.createElement('p');
+        description.classList.add('description-title');
+        description.textContent = exp.description;
+        experienceDiv.appendChild(description);
+
+        
+        
+        
+        
+        
         
         })
          
     } else {
         console.log(`cvResponse = ${cvResponse.status}`)
+        cvContent.innerHTML = '';
     }
     
 }
