c918078e68ef4b4af16ba7e24a7fd89b01bc9b1c
jerryChas <jerry.olsson@chasacademy.se>
2023-11-21 12:45:19 +0100

    started to get CV

--- js/cv.js
+++ js/cv.js
@@ -1,27 +1,43 @@
 //hämta element
 const cvContent = document.querySelector('.cv-content')
 
-//anropar funktion
-getCV()
+
+//*FUNKTIONER
+
+
 
 //Hämta json
 async function getCV() {
-    console.log('inside getCV')
+    
     const cvResponse = await fetch('./data/cv.json')
-
+    
     //Kontrollera att responsen är OK
     if (cvResponse.ok) {
-        const json = await cvResponse.json();
-        console.log(json)
+        const jsonData = await cvResponse.json();
+        console.log(typeof jsonData)
+        console.log(jsonData)
+        
+        jsonData.forEach(function(object){
 
+        //skapa container som ska hålla informationen
+        const experienceDiv = document.createElement('div')
+        experienceDiv.classList.add('experience_div');
 
+        //Hämtar data om speakers och skickar in i Diven per key
+        const experienceTitle = document.createElement('p');
+        experienceTitle.classList.add('experience-title');
+        experienceTitle.textContent = object.experience.title;
+        experienceDiv.appendChild(experienceTitle);
+        
+        })
+         
+    } else {
+        console.log(`cvResponse = ${cvResponse.status}`)
     }
-
-
-
-
-
+    
 }
 
 
+//anropar funktion
+getCV()
 
