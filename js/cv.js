11d806a50d9f8ebb23a5108b3d4d5678d4363063
jerryChas <jerry.olsson@chasacademy.se>
2023-11-22 09:37:14 +0100

    created and appended "educaation" to cv-content

--- js/cv.js
+++ js/cv.js
@@ -4,18 +4,11 @@ const cvContent = document.querySelector('.cv-content')
 
 //*FUNKTIONER
 
-function createElementFromJSON(jsonSource, classNameDiv, classNameP ){
-     //skapa container som ska hålla informationen
-     const divContainer = document.createElement('div')
-     divContainer.classList.add(classNameDiv);
-
-     //Hämtar data om speakers och skickar in i Diven per key
-     const pContainer = document.createElement('p');
-     pContainer.classList.add(classNameP);
-     // experienceTitle.textContent = object.experience.title;
-     
-     console.log(jsonSource)
-    //  container.appendChild();
+function createAndAppendElement(parentElement, HTMLelement, content, className) {
+    const element = document.createElement(HTMLelement);
+    element.classList.add(className);
+    element.textContent = content;
+    parentElement.appendChild(element);
 }
 
 
@@ -30,35 +23,36 @@ async function getCV() {
         console.log(typeof jsonData)
         console.log(jsonData)
         
-
-        
-        jsonData.experience.forEach(function(exp){
-
-   
+        //Hämtar container som ska fyllas med kommande information
         const experienceDiv = document.querySelector('.experience_div')
+        const educationDiv = document.querySelector('.education_div')
         
-
-        //Hämtar data om speakers och skickar in i Diven per key
-        const experienceTitle = document.createElement('h3');
-        experienceTitle.classList.add('experience-title');
-        experienceTitle.textContent = exp.title;
-        experienceDiv.appendChild(experienceTitle);
-
-        const description = document.createElement('p');
-        description.classList.add('description-title');
-        description.textContent = exp.description;
-        experienceDiv.appendChild(description);
-
-        
-        
-        
-        
-        
-        
+        jsonData.experience.forEach(function(obj){
+        //skapar element 
+        createAndAppendElement(experienceDiv, 'h3', obj.title, 'experience-title')
+        createAndAppendElement(experienceDiv, 'h4', obj.company, 'experience-company')
+        createAndAppendElement(experienceDiv, 'p', obj.period, 'experience-period')
+        createAndAppendElement(experienceDiv, 'p', obj.description, 'experience-description')
+        // const description = document.createElement('p');
+        // description.classList.add('experience-description');
+        // description.textContent = obj.description;
+        // experienceDiv.appendChild(description);
         })
+        jsonData.education.forEach(function(obj){
+            //skapar element 
+            createAndAppendElement(educationDiv, 'h3', obj.degreeName, 'education-degreeName')
+            createAndAppendElement(educationDiv, 'h4', obj.school, 'education-school')
+            createAndAppendElement(educationDiv, 'h5', obj.location, 'education-location')
+            createAndAppendElement(educationDiv, 'p', obj.period, 'education-period')
+            createAndAppendElement(educationDiv, 'p', obj.description, 'education-description')
+            
+        })
+            
+        
          
     } else {
         console.log(`cvResponse = ${cvResponse.status}`)
+        //tömmer cv-content
         cvContent.innerHTML = '';
     }
     
