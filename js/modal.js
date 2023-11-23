de5cbb4ca32ed80788b7204611b303a4914c9ac5
jerryChas <jerry.olsson@chasacademy.se>
2023-11-23 14:04:26 +0100

    Implement dynamic content based on clicked card

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,31 @@
 //KÄLLA: https://codepen.io/codejockie/pen/NBwjyd
 
+//Hämtar element
+const modalHeadline = document.querySelector('.modal-headline')
+const modalTextInfo = document.querySelector('.modal-text-info')
+
 //ändrar variablerna till const istället för var
 const modal = document.querySelector(".modal");
 const closeButton = document.querySelector(".close-button");
+
 //ändrar så att trigger är självaste korten i portfolion
-const trigger = document.querySelector(".card");
+const triggers = document.querySelectorAll(".card");
+
+// Loopar igenom varje trigger (kort) och lägger till en click-event lyssnare
+triggers.forEach(function(trigger) {
+    trigger.addEventListener("click", function(){
+        //Hämtar element för det specifika klickade kortet
+        const cardHeadline = trigger.querySelector('.card-headline')
+        const cardTextInfo = trigger.querySelector('.card-text-info')
+        
+        // Byter innehåll i modalen med innehållet från det klickade kortet
+        modalHeadline.textContent = `${cardHeadline.textContent}`;
+        modalTextInfo.textContent = `${cardTextInfo.textContent}`;
+
+        // Visar eller döljer modalen
+        toggleModal()
+    });
+});
 
 function toggleModal() {
     modal.classList.toggle("show-modal");
@@ -16,6 +37,5 @@ function windowOnClick(event) {
     }
 }
 
-trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
