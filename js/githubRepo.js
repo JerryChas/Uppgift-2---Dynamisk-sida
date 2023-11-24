eb2a2d452ab90ff013d880f9f14ac4981777b0c4
jerryChas <jerry.olsson@chasacademy.se>
2023-11-24 22:48:07 +0100

    generate cards from json

--- js/githubRepo.js
+++ js/githubRepo.js
@@ -12,6 +12,36 @@ async function getRepos() {
         console.log(typeof jsonData)
         console.log(jsonData)
 
+        //loopar datan från json
+        jsonData.forEach(function(obj) {
+        
+        //* GENERERAR KORT
+        //skapar kort
+        const card = document.createElement('div');
+        card.classList.add('card');
+        cardContainer.appendChild(card);
+        
+        //Lägger till en rubrik i kortet
+        const cardHeadline = document.createElement('h3');
+        cardHeadline.classList.add('card-headline');
+        cardHeadline.textContent = obj.name;
+        card.appendChild(cardHeadline)
+        
+        //lägger till en info-text i kortet
+        const cardTextInfo = document.createElement('p');
+        cardTextInfo.classList.add('card-text-info');
+        cardTextInfo.textContent = obj.description;
+        card.appendChild(cardTextInfo)
+        
+
+        // console.log(obj.name)
+        // console.log(obj.description)
+        // console.log(obj.homepage)
+        // console.log(obj.html_url)
+        
+        });
+
+
     } else {
         console.log(`repoResponse = ${repoResponse.status}`)
         //skriver ut felmeddelande
