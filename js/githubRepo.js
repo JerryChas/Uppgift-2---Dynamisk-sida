0c5595ec5fde7fd5e5d3a83fc7b171f6ecd81f77
jerryChas <jerry.olsson@chasacademy.se>
2023-11-24 23:50:57 +0100

    only show repo w/ topic "show"

--- js/githubRepo.js
+++ js/githubRepo.js
@@ -1,53 +1,50 @@
 //hämta element
-const cardContainer = document.querySelector('.card-container')
+const cardContainer = document.querySelector('.card-container');
 
 //Hämtar JSON
 async function getRepos() {
+  const repoResponse = await fetch(
+    'https://api.github.com/users/jerrychas/repos'
+  );
 
-    const repoResponse = await fetch('https://api.github.com/users/jerrychas/repos');
+  //Kontrollera att responsen är OK
+  if (repoResponse.ok) {
+    const jsonData = await repoResponse.json();
+    console.log(jsonData);
 
-    //Kontrollera att responsen är OK
-    if (repoResponse.ok) {
-        const jsonData = await repoResponse.json();
-        console.log(typeof jsonData)
-        console.log(jsonData)
-
-        //loopar datan från json
-        jsonData.forEach(function(obj) {
-        
-        //* GENERERAR KORT
+    //loopar datan från json
+    jsonData.forEach(function (obj) {
+      //* GENERERAR KORT
+      if (obj.topics == 'show') {
         //skapar kort
         const card = document.createElement('div');
         card.classList.add('card');
         cardContainer.appendChild(card);
-        
+
         //Lägger till en rubrik i kortet
         const cardHeadline = document.createElement('h3');
         cardHeadline.classList.add('card-headline');
         cardHeadline.textContent = obj.name;
-        card.appendChild(cardHeadline)
-        
+        card.appendChild(cardHeadline);
+
         //lägger till en info-text i kortet
         const cardTextInfo = document.createElement('p');
         cardTextInfo.classList.add('card-text-info');
         cardTextInfo.textContent = obj.description;
-        card.appendChild(cardTextInfo)
+        card.appendChild(cardTextInfo);
+
         
-
-        // console.log(obj.name)
-        // console.log(obj.description)
-        // console.log(obj.homepage)
-        // console.log(obj.html_url)
-        
-        });
-
-
-    } else {
-        console.log(`repoResponse = ${repoResponse.status}`)
-        //skriver ut felmeddelande
-        cardContainer.innerHTML = `<h2>${repoResponse.status} Kunde inte ladda projekt</h2>`;
-    }
-
+        console.log(obj.name)
+        console.log(obj.description)
+        console.log(obj.homepage)
+        console.log(obj.html_url)
+      }
+    });
+  } else {
+    console.log(`repoResponse = ${repoResponse.status}`);
+    //skriver ut felmeddelande
+    cardContainer.innerHTML = `<h2>${repoResponse.status} Kunde inte ladda projekt</h2>`;
+  }
 }
 
-getRepos()
+getRepos();
